5ac29e98e4f14ab3e353e04b8b08763180234404
Ahmed Abu Seada <168019079+ahmedaboseada@users.noreply.github.com>
2024-11-16 20:12:43 +0200

    Update server.js

--- server.js
+++ server.js
@@ -1,8 +1,8 @@
 const app = require('./app')
-const http = require('http')
-const server = http.createServer(app)
-server.listen(80, () => {
-    console.log(`Server started on port ${server.address().port}!`)
-})
+//const http = require('http')
+//const server = http.createServer(app)
+//server.listen(80, () => {
+//    console.log(`Server started on port ${server.address().port}!`)
+//})
 
 module.exports = app
